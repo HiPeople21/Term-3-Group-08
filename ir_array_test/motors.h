3854a2195f10b743be4324bda1dedb685141e6a8
Noah P <noahpigott1@gmail.com>
2026-05-27 14:57:23 +0100

    Code to follow lines, plant seeds

--- ir_array_test/motors.h
+++ ir_array_test/motors.h
@@ -1,7 +1,11 @@
 #pragma once
 #include <Arduino.h>
+#include <Motoron.h>
+
 
 void initMotors();
 void setRightTrack(int speed);
 void setLeftTrack(int speed);
+void setPlanter(int speed);
+
 void stopTracks();
